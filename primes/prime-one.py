41d7db7ec008c2a70071c1a5657cd075e2f23d45
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-06-15 12:36:59 +0530

    Prime up

--- primes/prime-one.py
+++ primes/prime-one.py
@@ -13,9 +13,18 @@ while starting_point <= m.sqrt(converted_input):
         else:
             prime_check = prime_check + 2
     starting_point = starting_point + 1
-
+    
+print(f'Number of factors: {prime_check}')
 
 if(prime_check == 2):
     print(f'{converted_input} is Prime')
 else:
     print(f'{converted_input} is not Prime')
+    
+'''
+Number of factors: 2
+4589756321 is Prime
+
+Number of factors: 12
+789456123 is not Prime
+'''
